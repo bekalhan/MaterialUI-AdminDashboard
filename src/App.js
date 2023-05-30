0e283dc4808d2584a4259c1b623cee93b6ab2798
root07 <77151270+bekalhan@users.noreply.github.com>
2023-05-30 01:32:27 -0700

    faq accordion added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 import { Routes,Route } from 'react-router-dom';
-// import FAQ from '/scenes/faq';
+import FAQ from './scenes/faq';
 // import Geography from './scenes/Geography';
 import Calendar from './scenes/calendar';
 
@@ -33,10 +33,10 @@ function App() {
                <Route path="/invoices" element={<Invoices />} />
                <Route path="/form" element={<Form />} />
                <Route path="/calendar" element={<Calendar />} />
+               <Route path="/faq" element={<FAQ />} />
              {/*  <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
                */}
 
---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,85 @@
+import {Accordion,AccordionSummary,AccordionDetails,Box,useTheme,Typography} from '@mui/material';
+import Header from '../../components/Header';
+import {ExpandMoreOutlined} from '@mui/icons-material';
+import {tokens} from '../../theme';
+
+
+const FAQ = () =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+
+    return(
+    <Box m="20px">
+        <Header title="FAQ" subtitle="Frequently Asked question Here" />
+        <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Favorite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            The Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+    )
+
+}
+
+export default FAQ;
